445572fd80ab5496bc9c8a426f9aa30c384c5090
Sourav Das <88787460+SouravDasHriday@users.noreply.github.com>
2026-06-27 05:00:05 +0600

    Update AMI filter values in main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,9 +1,11 @@
 data "aws_ami" "amazon-linux" {
   most_recent = true
 
+  owners = ["137112412989"]
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
