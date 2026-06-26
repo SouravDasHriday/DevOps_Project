7dd9fd0f95ea09f4744110440a3d0d178293e7b6
Sourav Das <88787460+SouravDasHriday@users.noreply.github.com>
2026-06-27 05:04:05 +0600

    Change key_name for AWS instance to '1st_key'

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name = "1st_key"
 
   tags = {
     Environment = "dev"
